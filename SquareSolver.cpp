553231223257b38e3e507980287748764631caef
satooomikk11 <varvara.gotovets@yandex.ru>
2025-08-16 01:14:14 +0300

    Проверка ввода, форма вывода, проверка NaN

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <math.h>
+#include <ctype.h>
 
 
-int sravn(double a, double b) {
-	if (fabs(a - b) > 0.000001) {
+int sravn(double a, double b) 
+{
+	if (fabs(a - b) > 0.000001) 
+	{
     	return 0; // Числа не равны
-	} else {
+	} 
+	else
+	{
 		return 1; // Числа равны
 	}
 }
 
 
-int main() {
+int main() 
+{
     double a = 1, b = 1, c = 1; // Коэффициенты
     double discriminant = 0, x1 = 0, x2 = 0;
 
@@ -19,44 +25,73 @@ int main() {
     
     // Проверка корректности ввода
     int read_count = scanf("%lf %lf %lf", &a, &b, &c);
-    if (read_count != 3) {
-        printf("Ошибка: введены некорректные данные. Требуются три числа.\n");
+	if (read_count != 3) 
+    {
+		printf("Ошибка: нужно ввести три числа\n");
+		return 1;
+    }
+
+
+    // Проверка NaN
+    if (isnan(a) || isnan(b) || isnan(c))
+    {
+        printf("Ошибка: введено значение NaN\n");
         return 1;
     }
-    int next_char = getchar();
-    if (next_char != '\n' && next_char != EOF) {
+
+	// Проверка лишних символов
+    int next_char;
+    while ((next_char = getchar()) != '\n' && next_char != EOF)
+    {
+        if (!isspace(next_char)) // Проверка пробела
+        {
         printf("Ошибка: после трёх чисел обнаружены лишние символы.\n");
-        return 1;
+            return 1;
+        }
     }
-    
+
     // Частные случаи
-    if (sravn(a, 0) == 1) {
-    	if (sravn(b, 0) == 1) {
-        	if (sravn(c, 0) == 1) {
+    if (sravn(a, 0) == 1) 
+    {
+    	if (sravn(b, 0) == 1) 
+    	{
+        	if (sravn(c, 0) == 1) 
+        	{
        		 printf("Бесконечное кол-во корней\n");
-      	  } else {
+      	  } 
+      	  else 
+      	  {
             	printf("Корней нет\n");
             }
-        } else {
+        } 
+        else 
+        {
             x1 = -c / b;
-        	printf("Единственный корень:\n");
-      	  printf("x = %E\n", x1);
+        	printf("Единственный корень:\n"
+      	  		  "x = %g\n", x1);;
         }
-    } else {
+	} 
+	else 
+	{
     	discriminant = b * b - 4 * a * c;
  
 		// Проверка дискриминанта
-		if (discriminant > 0) {
+		if (discriminant > 0) 
+		{
         	x1 = (-b - sqrt(discriminant)) / (2 * a);
         	x2 = (-b + sqrt(discriminant)) / (2 * a);
-        	printf("Два действительных корня:\n");
-        	printf("x1 = %E\n", x1);
-        	printf("x2 = %E\n", x2);
-        } else if (sravn(discriminant, 0) == 1) {
+        	printf("Два действительных корня\n"
+       			   "x1 = %g\n"
+        			  "x2 = %g\n", x1, x2);
+        } 
+        else if (sravn(discriminant, 0) == 1) 
+        {
         	x1 = -b / (2 * a);
-        	printf("Один действительный корень:\n");
-        	printf("x = %E\n", x1);
-        } else {
+        	printf("Один действительный корень:\n"
+        			  "x = %g\n", x1);
+        } 
+        else 
+        {
 			printf("Корней нет\n");
         }
     }
