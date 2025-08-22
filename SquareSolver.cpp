8ea12f2524e30770150620edc7b6e7adf7676327
satooomikk11 <varvara.gotovets@yandex.ru>
2025-08-22 20:16:25 +0300

    Enum-ы, деление по функциям

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -2,39 +2,47 @@
 #include <math.h>
 #include <ctype.h>
 #include <assert.h>
-#include <TXLib.h>
 
 
-// Объявление фукций
+// Объявление enum
+typedef enum {
+    SOLUTION_SUCCESS = 0,
+    SOLUTION_INVALID_TYPE = 1
+} SolutionResult;
+
+typedef enum {
+    INFINITE_SOLUTIONS = 0,
+    NO_SOLUTIONS = 1,
+    SINGLE_SOLUTION = 2,
+    TWO_REAL_SOLUTIONS = 3,
+    SINGLE_SOLUTION_ALT = 4
+} SolutionType;
+
+
+// Объявление функций
 int are_numbers_equal(double a, double b);
-int solution_output(int solution_type, double x1, double x2);
-int linear_equation_solver(double a, double b, double c, double *x);
-int square_equation_solver(double a, double b, double c, double *x1, double *x2);
+SolutionResult solution_output(SolutionType solution_type, double variables[]);
+SolutionType linear_equation_solver(double b, double c, double variables[]);
+SolutionType square_equation_solver(double variables[]);
 int check_extra_chars(void);
-int entering_coefficients_and_checking(double *a, double *b, double *c);
+int entering_coefficients_and_checking(double variables[]);
+SolutionType coosing_solution_type(double variables[]);
 
 
 int main()
 {
-    double a = 0, b = 0, c = 0; // Коэффициенты
-    double x1 = 0, x2 = 0; // Корни
+    // Массив для хранения всех переменных: [a, b, c, x1, x2]
+    double variables[5] = {0}; // Инициализируем все элементы нулями
 
-    if (entering_coefficients_and_checking(&a, &b, &c) != 0)
+    if (entering_coefficients_and_checking(variables) != 0)
     {
         printf("Программа завершена из-за ошибки ввода.\n");
         return 1;
     }
 
-    // Сначала пробуем решить как линейное уравнение
-    int solution_type = linear_equation_solver(a, b, c, &x1);
+    SolutionType solution_type = coosing_solution_type(variables);
 
-    // Если linear_equation_solver вернул -1, значит это квадратное уравнение
-    if (solution_type == -1)
-    {
-        solution_type = square_equation_solver(a, b, c, &x1, &x2);
-    }
-
-    solution_output(solution_type, x1, x2);
+    solution_output(solution_type, variables);
 
     return 0;
 }
@@ -53,94 +61,101 @@ int are_numbers_equal(double a, double b)
 }
 
 
-int solution_output(int solution_type, double x1, double x2)
+SolutionType coosing_solution_type(double variables[])
+{
+    if (are_numbers_equal(variables[0], 0) == 1)
+    {
+        return linear_equation_solver(variables[1], variables[2], variables);
+    }
+    else
+    {
+        return square_equation_solver(variables);
+    }
+}
+
+
+SolutionResult solution_output(SolutionType solution_type, double variables[])
 {
     switch(solution_type)
     {
         default:
             printf("Неверный solution_type!\n");
-            return 1;
+            return SOLUTION_INVALID_TYPE;
             break;
-        case 0:
+        case INFINITE_SOLUTIONS:
             printf("Бесконечное кол-во корней\n");
             break;
-        case 1:
+        case NO_SOLUTIONS:
             printf("Корней нет\n");
             break;
-        case 2:
-        case 4:
+        case SINGLE_SOLUTION:
+        case SINGLE_SOLUTION_ALT:
             printf("Единственный корень:\n"
-                      "x = %lg\n", x1);
+                      "x = %lg\n", variables[3]); // x1
             break;
-        case 3:
+        case TWO_REAL_SOLUTIONS:
             printf("Два действительных корня\n"
                       "x1 = %lg\n"
-                      "x2 = %lg\n", x1, x2);
+                      "x2 = %lg\n", variables[3], variables[4]); // x1, x2
             break;
     }
-    return 0;
+    return SOLUTION_SUCCESS;
 }
 
 
-int linear_equation_solver(double a, double b, double c,
-                           double *x1)
+SolutionType linear_equation_solver(double b, double c, double variables[])
 {
-    assert(x1 != NULL);
-    *x1 = 0;
+    // variables[0] = a, variables[1] = b, variables[2] = c
+    // variables[3] = x1, variables[4] = x2
 
     // Частные случаи
-    if (are_numbers_equal(a, 0) == 1)
+    if (are_numbers_equal(b, 0) == 1)
     {
-        if (are_numbers_equal(b, 0) == 1)
+        if (are_numbers_equal(c, 0) == 1)
         {
-            if (are_numbers_equal(c, 0) == 1)
-            {
-                return 0; // Бесконечное количество решений
-            }
-            else
-            {
-                return 1; // Нет решений
-            }
+            return INFINITE_SOLUTIONS; // Бесконечное количество решений
         }
         else
         {
-            *x1 = -c / b;
-            return 2; // Одно решение
+            return NO_SOLUTIONS; // Нет решений
         }
     }
-    return -1;
+    else
+    {
+        variables[3] = -c / b; // x1 = -c / b
+        return SINGLE_SOLUTION; // Одно решение
+    }
 }
 
 
-int square_equation_solver(double a, double b, double c,
-                           double *x1, double *x2)
+SolutionType square_equation_solver(double variables[])
 {
-    assert(are_numbers_equal(a, 0) == 0);
-    assert(x1 != NULL);
-    assert(x2 != NULL);
+    // variables[0] = a, variables[1] = b, variables[2] = c
+    // variables[3] = x1, variables[4] = x2
+
+    assert(are_numbers_equal(variables[0], 0) == 0); // Проверка что a ≠ 0
 
     double discriminant = 0;
-    *x1 = 0;
-    *x2 = 0;
+    variables[3] = 0; // x1
+    variables[4] = 0; // x2
 
-
-    discriminant = b * b - 4 * a * c;
+    discriminant = variables[1] * variables[1] - 4 * variables[0] * variables[2];
 
     // Проверка дискриминанта
     if (discriminant > 0)
     {
-        *x1 = (-b - sqrt(discriminant)) / (2 * a);
-        *x2 = (-b + sqrt(discriminant)) / (2 * a);
-        return 3; // Два решения
+        variables[3] = (-variables[1] - sqrt(discriminant)) / (2 * variables[0]); // x1
+        variables[4] = (-variables[1] + sqrt(discriminant)) / (2 * variables[0]); // x2
+        return TWO_REAL_SOLUTIONS; // Два решения
     }
     else if (are_numbers_equal(discriminant, 0) == 1)
     {
-        *x1 = -b / (2 * a);
-        return 4; // Одно решение (D = 0)
+        variables[3] = -variables[1] / (2 * variables[0]); // x1
+        return SINGLE_SOLUTION_ALT; // Одно решение (D = 0)
     }
     else
     {
-        return 1; // Нет решений
+        return NO_SOLUTIONS; // Нет решений
     }
 }
 
@@ -160,24 +175,22 @@ int check_extra_chars(void)
 }
 
 
-int entering_coefficients_and_checking(double *a, double *b, double *c)
+int entering_coefficients_and_checking(double variables[])
 {
-    assert(a != NULL);
-    assert(b != NULL);
-    assert(c != NULL);
+    // variables[0] = a, variables[1] = b, variables[2] = c
 
     printf("Введите коэффициенты a, b и c: ");
 
     // Ввод и проверка чисел
-    int read_count = scanf("%lf %lf %lf", a, b, c);
+    int read_count = scanf("%lf %lf %lf", &variables[0], &variables[1], &variables[2]);
     if (read_count != 3)
     {
         printf("Ошибка: нужно ввести три числа\n");
         return 1;
     }
-    
+
     // Проверка NaN
-    if (isnan(*a) || isnan(*b) || isnan(*c))
+    if (isnan((double) variables[0]) || isnan((double) variables[1]) || isnan((double) variables[2]))
     {
         printf("Ошибка: введено значение NaN\n");
         return 1;
