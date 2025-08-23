1bb6c3140f5ea3dfe59de4d960516d4ba2b81683
satooomikk11 <varvara.gotovets@yandex.ru>
2025-08-23 18:27:44 +0300

    Working with cli

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -4,42 +4,33 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <string.h>
+#include <TXLib.h>
 
-// todo enums (no typedef)
-// todo coosing -> choosing
-// todo asserts where necessary
-// todo assign to eq->type inside choosing_solution_type function
-// todo rename choosing_solution_type into solve_...
-// todo rename are_doubles_equal()
-// todo create function swap_double(double *, double *)
-// todo check x1 and x2 as well in tests where only type changes
-// todo 1e-10 -> const
+#include "wtf.h"
 
-// todo --self-check cli option
-// todo --solve cli opt
-// todo --help cli option
+const double MIN_DIFF = 1e-10;
 
 // enum declaration
-
-// new comment
-
-typedef enum {
+enum SolutionResult
+{
     SOLUTION_SUCCESS = 0,
     SOLUTION_INVALID_TYPE = 1
-} SolutionResult;
+};
 
 
-typedef enum {
+enum SolutionType
+{
     INFINITE_SOLUTIONS = 0,
     NO_SOLUTIONS = 1,
     SINGLE_SOLUTION = 2,
     TWO_REAL_SOLUTIONS = 3,
-    SINGLE_SOLUTION_ALT = 4
-} SolutionType;
+    SINGLE_SOLUTION_D0 = 4
+};
 
 
 // EQUATION STRUCTURE
-struct Equation {
+struct Equation
+{
     double a;
     double b;
     double c;
@@ -50,55 +41,88 @@ struct Equation {
 
 
 // function declaration
-int are_numbers_equal(double a, double b);
+int are_doubles_equal(double a, double b);
+void swap_double(double *a, double *b);
+
 int check_extra_chars(void);
 int entering_coefficients_and_checking(struct Equation *eq);
+
 SolutionResult solution_output(const struct Equation *eq);
-SolutionType coosing_solution_type(struct Equation *eq);
+SolutionType solve_equation(struct Equation *eq);
 SolutionType linear_equation_solver(struct Equation *eq);
 SolutionType square_equation_solver(struct Equation *eq);
-void unittest_solvesquare(void);
 
+void unittest_solvesquare(void);
 
 
 int main(int argc, char *argv[])
 {
-    printf("argc = %d\n", argc);
-    printf("argv[0] = %s\n", argv[0]);
-    printf("argv[1] = %s\n", argv[1]);
-    printf("argv[2] = %s\n", argv[2]);
+    Equation eq = {0, 0, 0, 0, 0, NO_SOLUTIONS}; // initialization
 
-    if (strcmp(argv[1], "a1") == 0) {
-        printf("first arg is a1!!!\n");
-    } else {
-        printf("not a1\n");
-    }
-
-    struct Equation eq = {0, 0, 0, 0, 0, NO_SOLUTIONS}; // initialization
-
-    // UNITESTING DED
-    unittest_solvesquare();
-
-    if (entering_coefficients_and_checking(&eq) != 0)
+    if (argc != 1)
     {
-        printf("The program is terminated due to an input error.\n");
-        return 1;
+        if (strcmp(argv[1], "--self-check") == 0)
+        {
+            unittest_solvesquare();
+        }
+
+        else if (strcmp(argv[1], "--solve") == 0)
+        {
+            if (entering_coefficients_and_checking(&eq) != 0)
+            {
+                printf("The program is terminated due to an input error.\n");
+                return 1;
+            }
+
+            solve_equation(&eq);
+            solution_output(&eq);
+        }
+
+        else if(strcmp(argv[1], "--help") == 0)
+        {
+            printf("BRIEF DESCRIPTION\n\n"
+                   "the program solves quadratic equations\n\n\n"
+                   "DETAILED DESCRIPTION\n\n"
+                   "--self-check performs self-tests (unit-tests)\n"
+                   "--solve solves quadratic equations\n\n");
+        }
     }
 
-    eq.type = coosing_solution_type(&eq);
-    solution_output(&eq);
+    else
+    {
+        Equation eq = {0, 0, 0, 0, 0, NO_SOLUTIONS}; // initialization
 
+        // UNITTESTING DED
+        unittest_solvesquare();
+
+        if (entering_coefficients_and_checking(&eq) != 0)
+        {
+            printf("The program is terminated due to an input error.\n");
+            return 1;
+        }
+
+        solve_equation(&eq);
+        solution_output(&eq);
+    }
 
     return 0;
 }
 
 
-
-int are_numbers_equal(double a, double b)
+int are_doubles_equal(double a, double b)
 {
-    return fabs(a - b) < 1e-10;
+    return fabs(a - b) < MIN_DIFF;
 }
 
+
+void swap_double(double *a, double *b)
+{
+    if (*a > *b) {
+        double temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+}
 
 
 int check_extra_chars(void)
@@ -119,6 +143,7 @@ int check_extra_chars(void)
 
 int entering_coefficients_and_checking(struct Equation *eq)
 {
+    assert(eq != NULL);
     printf("Enter the coefficients a, b and c: ");
 
     int read_count = scanf("%lf %lf %lf", &eq->a, &eq->b, &eq->c);
@@ -142,6 +167,7 @@ int entering_coefficients_and_checking(struct Equation *eq)
 
 SolutionResult solution_output(const struct Equation *eq)
 {
+    assert(eq != NULL);
     switch(eq->type)
     {
         default:
@@ -157,13 +183,13 @@ SolutionResult solution_output(const struct Equation *eq)
             break;
 
         case SINGLE_SOLUTION:
-        case SINGLE_SOLUTION_ALT:
+        case SINGLE_SOLUTION_D0:
             printf("The only solution:\n"
                    "x = %lg\n", eq->x1);
             break;
 
         case TWO_REAL_SOLUTIONS:
-            printf("Two valid solutions:\n"
+            printf("Two real solutions:\n"
                    "x1 = %lg\n"
                    "x2 = %lg\n", eq->x1, eq->x2);
             break;
@@ -173,25 +199,28 @@ SolutionResult solution_output(const struct Equation *eq)
 
 
 
-SolutionType coosing_solution_type(struct Equation *eq)
+SolutionType solve_equation(struct Equation *eq)
 {
-    if (are_numbers_equal(eq->a, 0))
+    assert(eq != NULL);
+    if (are_doubles_equal(eq->a, 0))
     {
-        return linear_equation_solver(eq);
+        eq->type = linear_equation_solver(eq);
     }
     else
     {
-        return square_equation_solver(eq);
+        eq->type = square_equation_solver(eq);
     }
+    return eq->type;
 }
 
 
 
 SolutionType linear_equation_solver(struct Equation *eq)
 {
-    if (are_numbers_equal(eq->b, 0))
+    assert(eq != NULL);
+    if (are_doubles_equal(eq->b, 0))
     {
-        if (are_numbers_equal(eq->c, 0))
+        if (are_doubles_equal(eq->c, 0))
         {
             return INFINITE_SOLUTIONS;
         }
@@ -211,7 +240,8 @@ SolutionType linear_equation_solver(struct Equation *eq)
 
 SolutionType square_equation_solver(struct Equation *eq)
 {
-    assert(!are_numbers_equal(eq->a, 0));
+    assert(eq != NULL);
+    assert(!are_doubles_equal(eq->a, 0));
 
     double discriminant = eq->b * eq->b - 4 * eq->a * eq->c;
 
@@ -222,10 +252,10 @@ SolutionType square_equation_solver(struct Equation *eq)
         eq->x2 = (-eq->b + sqrt(discriminant)) / (2 * eq->a);
         return TWO_REAL_SOLUTIONS;
     }
-    else if (are_numbers_equal(discriminant, 0))
+    else if (are_doubles_equal(discriminant, 0))
     {
         eq->x1 = -eq->b / (2 * eq->a);
-        return SINGLE_SOLUTION_ALT;
+        return SINGLE_SOLUTION_D0;
     }
     else
     {
@@ -248,26 +278,26 @@ void unittest_solvesquare(void)
 
     struct TestCase tests[] = {
         // 2 solutions
-        {1, -3, 2, TWO_REAL_SOLUTIONS, 1.0, 2.0, "Two solutions: x*82 - 3x + 2 = 0"},
-        {2, 5, -3, TWO_REAL_SOLUTIONS, -3.0, 0.5, "Two solutions: 2x**2 + 5x - 3 = 0"},
+        {1, -3,  2, TWO_REAL_SOLUTIONS,  1.0, 2.0,  "Two solutions:  x*82 - 3x + 2 = 0"},
+        {2,  5, -3, TWO_REAL_SOLUTIONS, -3.0, 0.5,  "Two solutions: 2x**2 + 5x - 3 = 0"},
 
         // 1 solution
-        {1, -4, 4, SINGLE_SOLUTION_ALT,  2.0,  2.0, "One solution:  x**2 - 4x + 4 = 0"},
-        {4,  4, 1, SINGLE_SOLUTION_ALT, -0.5, -0.5, "One solution: 4x**2 + 4x + 1 = 0"},
+        {1, -4, 4, SINGLE_SOLUTION_D0,  2.0,  2.0, "One solution:   x**2 - 4x + 4 = 0"},
+        {4,  4, 1, SINGLE_SOLUTION_D0, -0.5, -0.5, "One solution:  4x**2 + 4x + 1 = 0"},
 
         // 0 solutions
-        {1, 0, 1, NO_SOLUTIONS, 0, 0, "No solutions: x**2 + 1 = 0"},
-        {2, 2, 3, NO_SOLUTIONS, 0, 0, "No solutions: 2x**2 + 2x + 3 = 0"},
+        {1, 0, 1, NO_SOLUTIONS, 0, 0, "No solutions:   x**2 + 1 = 0"},
+        {2, 2, 3, NO_SOLUTIONS, 0, 0, "No solutions:  2x**2 + 2x + 3 = 0"},
 
         // a = 0
-        {0, 2, -4, SINGLE_SOLUTION, 2.0, 2.0, "Linear: 2x - 4 = 0"},
-        {0, 0, 5, NO_SOLUTIONS, 0, 0, "No solutions: 5 = 0"},
-        {0, 0, 0, INFINITE_SOLUTIONS, 0, 0, "Inf solutions: 0 = 0"},
-        {0, 3, 0, SINGLE_SOLUTION, 0.0, 0.0, "Linear: 3x = 0"},
+        {0, 2, -4, SINGLE_SOLUTION,    2.0, 2.0, "Linear:        2x - 4 = 0"},
+        {0, 0,  5, NO_SOLUTIONS,         0,   0, "No solutions:       5 = 0"},
+        {0, 0,  0, INFINITE_SOLUTIONS,   0,   0, "Inf solutions:      0 = 0"},
+        {0, 3,  0, SINGLE_SOLUTION,    0.0, 0.0, "Linear:        3x = 0"},
 
         // b, c = 0
-        {1, 0, -4, TWO_REAL_SOLUTIONS, -2.0, 2.0, "b = 0: x**2 - 4 = 0"},
-        {1, 0, 0, SINGLE_SOLUTION_ALT, 0.0, 0.0, "b = 0, c = 0: x**2 = 0"}
+        {1, 0, -4, TWO_REAL_SOLUTIONS, -2.0, 2.0, "b = 0:          x**2 - 4 = 0"},
+        {1, 0,  0, SINGLE_SOLUTION_D0,  0.0, 0.0, "b = 0, c = 0:   x**2 = 0"}
     };
 
     // Sizeof - operator, size in bytes
@@ -279,11 +309,11 @@ void unittest_solvesquare(void)
     for (int i = 0; i < total_tests; i++)
     {
         // Always put new test in structure
-        struct TestCase test = tests[i];
-        // Ð¡oefficients
-        struct Equation eq = {test.a, test.b, test.c};
+        TestCase test = tests[i];
+        // Ñoefficients
+        Equation eq = {test.a, test.b, test.c};
 
-        eq.type = coosing_solution_type(&eq);
+        eq.type = solve_equation(&eq);
 
         bool test_passed = false;
 
@@ -306,30 +336,22 @@ void unittest_solvesquare(void)
                     break;
 
                 case SINGLE_SOLUTION:
-                case SINGLE_SOLUTION_ALT:
-                    test_passed = are_numbers_equal(test.expected_x1, eq.x1);
+                case SINGLE_SOLUTION_D0:
+                    test_passed = are_doubles_equal(test.expected_x1, eq.x1);
                     break;
 
                 case TWO_REAL_SOLUTIONS:
                     // Sorting for comparing the solutions
                     double res1 = eq.x1;
                     double res2 = eq.x2;
-                    if (res1 > res2) {
-                        double temp = res1;
-                        res1 = res2;
-                        res2 = temp;
-                    }
+                    swap_double(&res1, &res2);
 
                     double exp1 = test.expected_x1;
                     double exp2 = test.expected_x2;
-                    if (exp1 > exp2) {
-                        double temp = exp1;
-                        exp1 = exp2;
-                        exp2 = temp;
-                    }
+                    swap_double(&exp1, &exp2);
 
-                    test_passed = are_numbers_equal(exp1, res1) &&
-                                  are_numbers_equal(exp2, res2);
+                    test_passed = are_doubles_equal(exp1, res1) &&
+                                  are_doubles_equal(exp2, res2);
                     break;
             }
         }
@@ -352,4 +374,3 @@ void unittest_solvesquare(void)
 
     printf("Result: %d/%d tests passed\n\n\n", passed_tests, total_tests);
 }
-
